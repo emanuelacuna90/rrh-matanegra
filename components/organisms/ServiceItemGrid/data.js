eb154a5df75b47bc337f639f33c21a9f1ec22842
acuna1990 <emanuel@wcanvas.com>
2024-01-30 13:06:07 -0300

    data

--- components/organisms/ServiceItemGrid/data.js
+++ components/organisms/ServiceItemGrid/data.js
@@ -2,22 +2,22 @@ export const dataItem = [
   {
     id: 1,
     title: `RECLUTAMIENTO Y SELECCIÓN DE PERSONAL
-    (MAPPING & RECRUITING)`,
+    (Mapping & Recruiting)`,
     text: 'Somos socios estratégicos en el desarrollo de una gran networking de profesionales y personal idóneo para cubrir las necesidades de la empresa con las aspiraciones de los candidatos y la realidad de mercado',
   },
   {
     id: 2,
-    title: `capacitacion, desarrollo $ clima laboral (TRAINING, DEVELOPMENT AND WORK CLIMATE)`,
+    title: `CAPACITACION, DESARROLLO & CLIMA LABORAL (Training, Development and Work Climate)`,
     text: 'Te ofrecemos desarrollar habilidades y aplicar conocimientos adquiridos mediante el diseño a medida de cursos y talleres teóricos/prácticos sobre diferentes temáticas hard y soft para que puedas aplicar en tu empresa, según las necesidades.',
   },
   {
     id: 3,
-    title: `GESTIÓN DE RELACIONES LABORALES Y GREMIALES (MANAGEMENT OF LABOR AND UNION RELATIONS) & tercerizacion DE nomina y LIQUIDACIÓN DE HABERES (PAYROLL OUTSOURCING & ASSET SETTLEMENT)`,
+    title: `GESTIÓN DE RELACIONES LABORALES Y GREMIALES (Management of Labor and Union Relations) & TERCIARIZACIÓN DE NÓMINA y LIQUIDACIÓN DE HABERES (Payroll Outsourcing & Asset Settlement)`,
     text: 'Realizamos el diseño ajustable a la necesidad y a todas las etapas de la liquidación de haberes relacionadas con las obligaciones previsionales, la información contable, los cumplimientos impositivos, garantizando la confidencialidad en el manejo de la información. Te ofrecemos nuestra experiencia en el área de capital humano con profesionales de alta especialización en soluciones de administración de personal, trámites, rúbricas, entre otros.',
   },
   {
     id: 4,
-    title: `MEDICINA LABORAL (OCCUPATIONAL MEDICINE)`,
+    title: `MEDICINA LABORAL (Occupational Medicine)`,
     text: 'Ofrecemos el servicio de medicina asistencial a través de un equipo interdisciplinario especializado cuyos profesionales gestionan de manera inherentes al servicio de Medicina Laboral establecidas legalmente en la resolución 905/15 de la Superintendencia  de Riesgos del Trabajo, como así también contemplar y dar respuesta a las necesidades de las empresas en Salud Ocupacional.',
   },
   {
